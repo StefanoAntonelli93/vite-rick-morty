b022090f8ac628c317168af52d8bdc766e25a9cd
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-06-04 17:51:19 +0200

    [UPD] create component 'CharacterElement.vue'

---
+++ src/components/CharacterElement.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: "CharacterElement",
+    // chiave props
+    props: ['image', 'name', 'status', 'species'],
+}
+</script>
+
+<template>
+    <div>
+        <img :src="image" :alt="name">
+        <h3> {{ name }}</h3>
+        <p>{{ status }}</p>
+        <p>{{ species }}</p>
+    </div>
+</template>
--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -1,8 +1,13 @@
 <script>
 // importo store
 import { store } from '../store';
+// importo Componente CharacterElement
+import CharacterElement from './CharacterElement.vue'
 export default {
     name: "CharacterList",
+    components: {
+        CharacterElement,
+    },
     data() {
         return {
             store,
@@ -20,10 +25,13 @@ export default {
 
     <!-- itero con ciclo v-for per ogni character mostro images, nome, status, species-->
     <div class="character" v-for="(character, index) in store.results.results" :key="index">
-        <img :src="character.image" :alt="character.name">
+        <CharacterElement :image="character.image" :name="character.name" :status="character.status"
+            :species="character.species" />
+
+        <!-- <img :src="character.image" :alt="character.name">
         <h3> {{ character.name }}</h3>
         <p>{{ character.status }}</p>
-        <p>{{ character.species }}</p>
+        <p>{{ character.species }}</p> -->
     </div>
 
 </template>
